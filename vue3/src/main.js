f290677ab57fb23a84c1c248b592f8d9f30e8bae
HaPpY <930333105@qq.com>
2025-08-08 18:47:46 +0800

    feat(vue3): 集成 Element Plus 组件库并添加国际化支持
    - 在 main.js 中引入 Element Plus 和中文语言包- 在 package.json 中添加 Element Plus 相关依赖
    - 更新项目配置，支持 Element Plus 组件自动导入

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -1,10 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import '@/assets/global.css'
+import {zhCn} from "element-plus/es/locale/index";
 
 const app = createApp(App)
 
 app.use(router)
+
+//element-plus 语言包，变成中文
+app.use(ElementPlus, {
+    locale: zhCn,
+})
 
 app.mount('#app')
