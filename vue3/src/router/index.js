4e4c9bf5abcc576e4220ded10472c12ecfe17d6f
HaPpY <930333105@qq.com>
2025-08-14 13:51:31 +0800

    fix(router): 修正管理员主菜单路由名称
    - 将路由名称 'manger' 修正为 'manager'，统一命名规范

--- vue3/src/router/index.js
+++ vue3/src/router/index.js
@@ -4,7 +4,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: '/manager'},
-    { path: '/manager', name: 'manger', meta: { title: '管理员主菜单' }, component: () => import('../views/manager.vue') },
+    { path: '/manager', name: 'manager', meta: { title: '管理员主菜单' }, component: () => import('../views/manager.vue') },
     { path: "/404", name: "404", meta: { title: "页面不存在" }, component: () => import("../views/404.vue")},
     { path: '/:pathMatch(.*)*', redirect: '/404'}
   ],
