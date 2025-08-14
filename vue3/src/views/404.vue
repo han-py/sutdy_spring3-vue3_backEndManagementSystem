c74b6b75a41c1823e45f1e2762456a2077aad26f
HaPpY <930333105@qq.com>
2025-08-14 10:50:47 +0800

    feat: 添加 404 页面
    - 新增 404.vue 文件，实现自定义 404 页面
    - 页面包含404 图片和返回主页按钮
    - 使用 Element UI 的 el-button 组件

---
+++ vue3/src/views/404.vue
@@ -0,0 +1,10 @@
+<template>
+  <div style="text-align: center">
+    <img src="@/assets/404.png" alt="">
+    <div><el-button type="primary" style="width: 150px; height: 60px; font-size: 20px" @click="goHome">返回主页</el-button></div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
