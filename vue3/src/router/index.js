55d097844ede2e3293f8e15cc70e3ff53bec5b73
HaPpY <930333105@qq.com>
2025-08-14 13:22:01 +0800

    feat(router): 添加 404 页面并更新路由配置
    - 新增 404 页面组件和对应的路由
    - 更新 README 文件中的路由配置说明
    - 添加全局未知路由重定向到 404 页面

--- vue3/src/router/index.js
+++ vue3/src/router/index.js
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: () => import('../views/Home.vue') },
+    { path: '/', name: 'manager', meta: { title: '管理员主页' }, component: () => import('../views/manger.vue') },
+    { path:'/404', name: 'notFound', meta: { title: '页面未找到' }, component: () => import('../views/404.vue') },
+    { path: '/:pathMatch(.*)*', redirect: '/404'}
   ],
 })
 
