309ce0aa48e9bf468a65ca59336245f2675531d2
HaPpY <930333105@qq.com>
2025-08-14 09:57:44 +0800

    feat(vue3): 自定义 element-plus 主题并按需加载组件

--- vue3/src/main.js
+++ vue3/src/main.js
@@ -5,6 +5,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import '@/assets/global.css'
 import {zhCn} from "element-plus/es/locale/index";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
@@ -14,5 +15,9 @@ app.use(router)
 app.use(ElementPlus, {
     locale: zhCn,
 })
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.mount('#app')
