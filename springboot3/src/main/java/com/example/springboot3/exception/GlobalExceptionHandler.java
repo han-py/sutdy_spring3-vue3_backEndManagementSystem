fa921248673173711c9afda7cb92b1568f6a7250
HaPpY <930333105@qq.com>
2025-08-15 10:45:16 +0800

    excep: 优化异常处理逻辑
    - 在全局异常处理器中增加异常打印，便于排查问题
    - 新增自定义异常处理方法，实现更细粒度的异常管理

--- springboot3/src/main/java/com/example/springboot3/exception/GlobalExceptionHandler.java
+++ springboot3/src/main/java/com/example/springboot3/exception/GlobalExceptionHandler.java
@@ -10,6 +10,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody // 返回json串
     public Result error(Exception e) {
+        e.printStackTrace();
         return Result.error();
+    }
+
+    @ExceptionHandler(CustomException.class)
+    @ResponseBody // 返回json串
+    public Result error(CustomException e) {
+        e.printStackTrace();
+        return Result.error(e.getCode(), e.getMsg());
     }
 }
